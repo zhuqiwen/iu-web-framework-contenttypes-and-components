52103fc112cd5f37b437e258201bf08eae1f4ef8
Qiwen Zhu <qiwzhu@iu.edu>
2025-12-18 09:33:31 -0500

    add Video

--- src/Section/Chunks/Video.php
+++ src/Section/Chunks/Video.php
@@ -8,16 +8,77 @@ use Edu\IU\RSB\StructuredDataNodes\GroupNode;
 class Video extends ChunkAbstract{
 
 
-    public readonly string $header;
-    public readonly string $headerLevel;
-    public readonly string $content;
+    //strings
+    public string $headerLevel;
+    public string $videoType;
+    public string $title;
+    public string $caption;
+    public string $transcript;
+
+    public string $url;
+    public string $urlDescribedVersion;
+
+    //assets
+    public string $vttId;
+    public string $vttPath;
+
+    public string $descriptionsFileId;
+    public string $descriptionsFilePath;
+
+    public string $thumbnailId;
+    public string $thumbnailPath;
+
+    public string $mp4Id;
+    public string $mp4Path;
+    public string $webmId;
+    public string $webmPath;
+    public string $mp4DescribedId;
+    public string $mp4DescribedPath;
+    public string $webmDescribedId;
+    public string $webmDescribedPath;
+
+
+
 
 
     public function fetchDataFromGroupNode(GroupNode $chunkDetails): void
     {
-        $this->header = $chunkDetails->getSingleDescendantNodeByPath('header')->text ?? '';
         $this->headerLevel = $chunkDetails->getSingleDescendantNodeByPath('header-level')->text ?? '';
-        $this->content = $chunkDetails->getSingleDescendantNodeByPath('content')->text ?? '';
-    }
+        $this->videoType = $chunkDetails->getSingleDescendantNodeByPath('video-type')->text ?? '';
+        $this->title = $chunkDetails->getSingleDescendantNodeByPath('title')->text ?? '';
+        $this->caption = $chunkDetails->getSingleDescendantNodeByPath('caption')->text ?? '';
+        $this->transcript = $chunkDetails->getSingleDescendantNodeByPath('transcript')->text ?? '';
 
+        $this->url = $chunkDetails->getSingleDescendantNodeByPath('url')->text ?? '';
+        $this->urlDescribedVersion = $chunkDetails->getSingleDescendantNodeByPath('video-id-described')->text ?? '';
+
+        $thumbnail = $chunkDetails->getSingleDescendantNodeByPath('thumbnail');
+        $this->thumbnailId = $thumbnail->fileId ?? '';
+        $this->thumbnailPath = $thumbnail->filePath ?? '';
+
+        $vtt = $chunkDetails->getSingleDescendantNodeByPath('vtt');
+        $this->vttId = $vtt->fileId ?? '';
+        $this->vttPath = $vtt->filePath ?? '';
+
+        $mp4 = $chunkDetails->getSingleDescendantNodeByPath('mp4');
+        $this->mp4Id = $mp4->fileId ?? '';
+        $this->mp4Path = $mp4->filePath ?? '';
+
+        $webm = $chunkDetails->getSingleDescendantNodeByPath('webm');
+        $this->webmId = $webm->fileId ?? '';
+        $this->webmPath = $webm->filePath ?? '';
+
+        $mp4Described = $chunkDetails->getSingleDescendantNodeByPath('mp4-described');
+        $this->mp4DescribedId = $mp4Described->fileId ?? '';
+        $this->mp4DescribedPath = $mp4Described->filePath ?? '';
+
+        $webmDescribed = $chunkDetails->getSingleDescendantNodeByPath('webm-described');
+        $this->webmDescribedId = $webmDescribed->fileId ?? '';
+        $this->webmDescribedPath = $webmDescribed->filePath ?? '';
+
+        $description = $chunkDetails->getSingleDescendantNodeByPath('descriptions-file');
+        $this->descriptionsFileId = $description->fileId ?? '';
+        $this->descriptionsFilePath = $description->filePath ?? '';
+
+    }
 }
